e49184247dc1067a4a3591ff266677eeb4ce44c2
John Doe <johndoe@example.com>
2023-01-24 14:15:24 +0300

    777

--- src/modules/render/renderProducts.js
+++ src/modules/render/renderProducts.js
@@ -9,63 +9,6 @@ export const renderProducts = () => {
 		<li class="goods__item">
 			<article class="product-card">
 				<a class="product-card__link" href="#">
-					<img class="product-card__img" src="./img/foto-card-1.jpg"
-						alt="Бюстгальтер-Балконет Bien из Микрофибры">
-					<h3 class="product-card__title">
-						Бюстгальтер-Балконет Bien из Микрофибры
-					</h3>
-				</a>
-				<div class="product-card__row">
-					<p class="product-card__price">руб 1799</p>
-					<button class="product-card__btn-favorite product-card__btn-favorite--active btn-reset"
-						aria-label="добавить в избранное"></button>
-				</div>
-
-				<ul class="product-card__color-list list-reset">
-					<li class="product-card__color-item">
-						<div class="color color--red color--check"></div>
-					</li>
-					<li class="product-card__color-item">
-						<div class="color color--white"></div>
-					</li>
-					<li class="product-card__color-item">
-						<div class="color color--black"></div>
-					</li>
-				</ul>
-			</article>
-		</li>
-
-		<li class="goods__item">
-			<article class="product-card">
-				<a class="product-card__link" href="#">
-					<img class="product-card__img" src="./img/foto-card-2.jpg"
-						alt="Бюстгальтер-Балконет Bien из Микрофибры">
-					<h3 class="product-card__title">
-						Бюстгальтер-Балконет Bien из Микрофибры
-					</h3>
-				</a>
-				<div class="product-card__row">
-					<p class="product-card__price">руб 1799</p>
-					<button class="product-card__btn-favorite btn-reset" aria-label="добавить в избранное"></button>
-				</div>
-
-				<ul class="product-card__color-list list-reset">
-					<li class="product-card__color-item">
-						<div class="color color--red color--check"></div>
-					</li>
-					<li class="product-card__color-item">
-						<div class="color color--white"></div>
-					</li>
-					<li class="product-card__color-item">
-						<div class="color color--black"></div>
-					</li>
-				</ul>
-			</article>
-		</li>
-
-		<li class="goods__item">
-			<article class="product-card">
-				<a class="product-card__link" href="#">
 					<img class="product-card__img" src="./img/foto-card-3.jpg"
 						alt="Бюстгальтер-Балконет Bien из Микрофибры">
 					<h3 class="product-card__title">
@@ -73,34 +16,7 @@ export const renderProducts = () => {
 					</h3>
 				</a>
 				<div class="product-card__row">
-					<p class="product-card__price">руб 1799</p>
-					<button class="product-card__btn-favorite btn-reset" aria-label="добавить в избранное"></button>
-				</div>
-
-				<ul class="product-card__color-list list-reset">
-					<li class="product-card__color-item">
-						<div class="color color--red color--check"></div>
-					</li>
-					<li class="product-card__color-item">
-						<div class="color color--white"></div>
-					</li>
-					<li class="product-card__color-item">
-						<div class="color color--black"></div>
-					</li>
-				</ul>
-			</article>
-		</li>
-		<li class="goods__item">
-			<article class="product-card">
-				<a class="product-card__link" href="#">
-					<img class="product-card__img" src="./img/foto-card-1.jpg"
-						alt="Бюстгальтер-Балконет Bien из Микрофибры">
-					<h3 class="product-card__title">
-						Бюстгальтер-Балконет Bien из Микрофибры
-					</h3>
-				</a>
-				<div class="product-card__row">
-					<p class="product-card__price">руб 1799</p>
+					<p class="product-card__price">1799 <strong>₽</strong></p>
 					<button class="product-card__btn-favorite btn-reset" aria-label="добавить в избранное"></button>
 				</div>
 
